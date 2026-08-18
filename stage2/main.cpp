6f62852908ee13509d9f2bd194b4825c8a0f3ae0
amerjyothb240135cs-lab <amerjyoth_b240135cs@nitc.ac.in>
2026-08-19 00:11:39 +0530

    Update main.cpp

--- stage2/main.cpp
+++ stage2/main.cpp
@@ -1,1 +1,57 @@
+#include "Disk_Class/Disk.h"
+#include "Cache/OpenRelTable.h"
+#include "Buffer/BlockBuffer.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Disk diskRun;
+
+    RecBuffer relCatBuffer(RELCAT_BLOCK);
+    RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+    HeadInfo relCatHeader;
+    HeadInfo attrCatHeader;
+
+    relCatBuffer.getHeader(&relCatHeader);
+    attrCatBuffer.getHeader(&attrCatHeader);
+
+    for (int i = 0; i < relCatHeader.numEntries; i = i + 1)
+    {
+        Attribute relCatRecord[RELCAT_NO_ATTRS];
+
+        relCatBuffer.getRecord(relCatRecord, i);
+
+        cout << "Relation: "<< relCatRecord[RELCAT_REL_NAME_INDEX].sVal << endl;
+
+        for (int j = 0; j < attrCatHeader.numEntries; j = j + 1)
+        {
+            Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+            attrCatBuffer.getRecord(attrCatRecord, j);
+
+            if (strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal,
+                       attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0)
+            {
+                cout << "  "<< attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal << ": ";
+        
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        cout << attrType << endl;    
+      }
+         /*if (attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER)
+                {
+                    cout << "NUM";
+                }
+                else
+                {
+                    cout << "STR";
+                }*/
+
+            }
+            cout << endl;
+        }
+return 0;
+    }
 
