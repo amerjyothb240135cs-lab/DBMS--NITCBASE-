c894c9c74b6b6fe4363ef228752eee28aad3e16c
amerjyothb240135cs-lab <amerjyoth_b240135cs@nitc.ac.in>
2026-08-19 00:24:34 +0530

    Update Blockbuffer.cpp

--- stage3/Blockbuffer.cpp
+++ stage3/Blockbuffer.cpp
@@ -1,1 +1,86 @@
+#include "BlockBuffer.h"
 
+#include <cstdlib>
+#include <cstring>
+
+BlockBuffer::BlockBuffer(int blockNum) {
+    this->blockNum = blockNum;
+}
+
+RecBuffer::RecBuffer(int blockNum)
+    : BlockBuffer(blockNum) {
+}
+
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+
+    unsigned char *bufferPtr;
+
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    memcpy(&head->numSlots, bufferPtr + 24, 4);
+    memcpy(&head->numEntries, bufferPtr + 16, 4);
+    memcpy(&head->numAttrs, bufferPtr + 20, 4);
+    memcpy(&head->rblock, bufferPtr + 12, 4);
+    memcpy(&head->lblock, bufferPtr + 8, 4);
+
+    return SUCCESS;
+}
+
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+
+    struct HeadInfo head;
+
+    int ret = getHeader(&head);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char *bufferPtr;
+
+    ret = loadBlockAndGetBufferPtr(&bufferPtr);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    int recordSize = attrCount * ATTR_SIZE;
+
+    unsigned char *slotPointer =
+        bufferPtr + HEADER_SIZE + slotCount +
+        (recordSize * slotNum);
+
+    memcpy(rec, slotPointer, recordSize);
+
+    return SUCCESS;
+}
+
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
+
+    int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+    if (bufferNum == E_BLOCKNOTINBUFFER) {
+
+        bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+        if (bufferNum == E_OUTOFBOUND) {
+            return E_OUTOFBOUND;
+        }
+
+        Disk::readBlock(
+            StaticBuffer::blocks[bufferNum],
+            this->blockNum
+        );
+    }
+
+    *buffPtr = StaticBuffer::blocks[bufferNum];
+
+    return SUCCESS;
+}
