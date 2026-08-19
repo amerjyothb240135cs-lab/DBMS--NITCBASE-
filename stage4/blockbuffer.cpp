8895253aa73f50219cacd09c9f3261e3d90c7a1b
amerjyothb240135cs-lab <amerjyoth_b240135cs@nitc.ac.in>
2026-08-19 22:08:47 +0530

    Create blockbuffer.cpp

---
+++ stage4/blockbuffer.cpp
@@ -0,0 +1,131 @@
+#include "BlockBuffer.h"
+
+#include <cstdlib>
+#include <cstring>
+
+BlockBuffer::BlockBuffer(int blockNum) {
+    this->blockNum = blockNum;
+}
+
+RecBuffer::RecBuffer(int blockNum)
+    : BlockBuffer(blockNum) {
+}
+
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+
+    unsigned char *bufferPtr;
+
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    memcpy(&head->numSlots, bufferPtr + 24, 4);
+    memcpy(&head->numEntries, bufferPtr + 16, 4);
+    memcpy(&head->numAttrs, bufferPtr + 20, 4);
+    memcpy(&head->rblock, bufferPtr + 12, 4);
+    memcpy(&head->lblock, bufferPtr + 8, 4);
+
+    return SUCCESS;
+}
+
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+
+    struct HeadInfo head;
+
+    int ret = getHeader(&head);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char *bufferPtr;
+
+    ret = loadBlockAndGetBufferPtr(&bufferPtr);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    int recordSize = attrCount * ATTR_SIZE;
+
+    unsigned char *slotPointer =
+        bufferPtr + HEADER_SIZE + slotCount +
+        (recordSize * slotNum);
+
+    memcpy(rec, slotPointer, recordSize);
+
+    return SUCCESS;
+}
+
+int RecBuffer::getSlotMap(unsigned char *slotMap) {
+    unsigned char *bufferPtr;
+
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    struct HeadInfo head;
+
+    ret = getHeader(&head);
+
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    int slotCount = head.numSlots;
+
+    unsigned char *slotMapInBuffer =
+        bufferPtr + HEADER_SIZE;
+
+    memcpy(slotMap, slotMapInBuffer, slotCount);
+
+    return SUCCESS;
+}
+
+int compareAttrs(union Attribute attr1, union Attribute attr2, int attrType) {
+    double diff;
+
+    if (attrType == STRING) {
+        diff = strcmp(attr1.sVal, attr2.sVal);
+    } else {
+        diff = attr1.nVal - attr2.nVal;
+    }
+
+    if (diff > 0)
+        return 1;
+
+    if (diff < 0)
+        return -1;
+
+    return 0;
+}
+
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
+
+    int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+    if (bufferNum == E_BLOCKNOTINBUFFER) {
+
+        bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+        if (bufferNum == E_OUTOFBOUND) {
+            return E_OUTOFBOUND;
+        }
+
+        Disk::readBlock(
+            StaticBuffer::blocks[bufferNum],
+            this->blockNum
+        );
+    }
+
+    *buffPtr = StaticBuffer::blocks[bufferNum];
+
+    return SUCCESS;
+}
